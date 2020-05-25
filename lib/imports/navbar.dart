c171372a37dfe7ec4c0d03cd475f263667ff0a68
Arınç Alp Eren <arinc.alp.98@gmail.com>
2020-05-26 01:41:06 +0300

    navbar :: navbar widget splitted

---
+++ lib/imports/navbar.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+class Navbar extends StatefulWidget{
+  @override
+  _NavbarState createState() => _NavbarState();
+}
+
+class _NavbarState extends State<Navbar> {
+  
+  int page = 0;
+
+  void changePage(int index){
+    setState(() {
+      page = index;
+    });
+    gotoPage(page);
+  }
+
+  Widget gotoPage(int page){
+    if(page == 0){
+      Navigator.pushReplacementNamed(context, '/memberpage');
+      print("0");
+    }
+    else if(page == 1){
+      Navigator.pushReplacementNamed(context, '/memberpage');
+      print("1");
+    }
+    else if(page == 2){
+      Navigator.pushReplacementNamed(context, '/memberpage');
+      print("2");
+    }
+    else if(page == 3){
+      Navigator.pushReplacementNamed(context, '/pairs');
+      print("3");
+    }
+  }
+
+  Widget build(BuildContext context){
+    return(
+      BottomNavigationBar(
+        currentIndex: page,
+        onTap: changePage,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 35,
+        fixedColor: Colors.black,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            title: SizedBox(
+              height: 0,
+            ),
+          ),
+        ],
+      )
+    );
+  }
+}
--- lib/pages/memberpage.dart
+++ lib/pages/memberpage.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import "package:flutter/material.dart";
+import 'package:newapp/imports/navbar.dart';
 
 class MemberPage extends StatefulWidget {
     @override
@@ -7,82 +7,14 @@ class MemberPage extends StatefulWidget {
 }
 
 class _MemberPageState extends State<MemberPage> {
-
-  int page = 0;
-  void changePage(int index){
-    setState(() {
-      page = index;
-    });
-    gotoPage(page);
-  }
-
-  Widget gotoPage(int page){
-    if(page == 0){
-      Navigator.pushReplacementNamed(context, '/register');
-    }
-    else if(page == 1){
-      Navigator.pushReplacementNamed(context, '/register');
-    }
-    else if(page == 2){
-      Navigator.pushReplacementNamed(context, '/register');
-    }
-    else if(page == 3){
-      Navigator.pushReplacementNamed(context, '/pairs');
-    }
-  }
-
-  // _MemberPageState({this.username});
-  // final String username;
-
+  Navbar navbar = new Navbar();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Sevgilim Nerede?"),),
       bottomNavigationBar: Theme(
-          data: Theme.of(context).copyWith(canvasColor: Colors.grey[100]),
-          child: BottomNavigationBar(
-            currentIndex: page,
-            onTap: changePage,
-            type: BottomNavigationBarType.fixed,
-            iconSize: 35,
-            fixedColor: Colors.black,
-            items: <BottomNavigationBarItem>[
-              BottomNavigationBarItem(
-                icon: Icon(Icons.settings),
-                title: SizedBox(
-                  height: 0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.location_on),
-                title: SizedBox(
-                  height: 0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                title: SizedBox(
-                  height: 0,
-                ),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                title: SizedBox(
-                  height: 0,
-                ),
-              ),
-            ],
-          ),
-        // children: <Widget>[
-          //Text('Hallo $username', style: TextStyle(fontSize: 20.0),),
-          // RaisedButton(
-          //   child: Text("Logout"),
-          //   onPressed: (){
-          //     Navigator.pushReplacementNamed(context,'/');
-          //   },
-          // ),
-        // ],
-
+        data: Theme.of(context).copyWith(canvasColor: Colors.grey[100]),
+        child: navbar,
       ),
       body: Column(
         
