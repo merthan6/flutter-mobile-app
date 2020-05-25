97ba7a967aa09201e5c2badd3d8527151220cfc9
Alperen Sarinay <alperensarinay@gmail.com>
2020-05-25 18:33:18 +0300

    Home :: Added Welcome page

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,16 @@ import 'package:flutter/material.dart';
 import 'package:newapp/pages/login.dart';
 import 'package:newapp/pages/memberpage.dart';
 import 'package:newapp/pages/register.dart';
+import 'package:newapp/pages/Home.dart';
 void main() => runApp(
   MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: '/',
+    initialRoute: '/Home',
     routes: {
         '/': (BuildContext context) => LoginPage(),
-        '/memberpage': (BuildContext context) => MemberPage(username: username,),
+        '/memberpage': (BuildContext context) => MemberPage(),
         '/register': (BuildContext context) => Register(),
+        '/Home': (BuildContext context) => Home(),
       },
   )
 );
---
+++ lib/pages/Home.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:newapp/pages/login.dart';
+import 'dart:async';
+
+
+class Home extends StatefulWidget{
+
+  State<StatefulWidget> createState(){
+    return StartState();
+  }
+}
+class StartState extends State<Home> {
+
+  @override
+  void initState(){
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async{
+    var duration = Duration(seconds: 2);
+    return Timer(duration, route);
+  }
+  route(){
+    Navigator.pushReplacementNamed(context, '/');
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[500],
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            SizedBox(height:200,),
+            Positioned(
+              child: Container(
+                height: 200,
+              decoration : BoxDecoration(
+                image : DecorationImage(
+                  image : AssetImage('assets/images/logo.png'),
+                )
+              )
+            )
+            ),
+            
+            SizedBox(height:30,),
+            CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              strokeWidth:2,
+            )
+          ] 
+      ),
+      ), 
+    );  
+  }
+  }
+
