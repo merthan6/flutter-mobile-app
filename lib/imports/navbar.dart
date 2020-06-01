bdfcb0fecd02941357992ae2b98ff230ef90623b
Alperen Sarinay <alperensarinay@gmail.com>
2020-06-01 23:07:11 +0300

    navbar :: bug fixed

--- lib/imports/navbar.dart
+++ lib/imports/navbar.dart
@@ -15,18 +15,14 @@ class _NavbarState extends State<Navbar> {
     });
     gotoPage(page);
   }
-
   Widget gotoPage(int page){
     if(page == 0){
       Navigator.pushReplacementNamed(context, '/memberpage');
     }
     else if(page == 1){
-      Navigator.pushReplacementNamed(context, '/memberpage');
-    }
-    else if(page == 2){
       Navigator.pushReplacementNamed(context, '/profile');
     }
-    else if(page == 3){
+    else if(page == 2){
       Navigator.pushReplacementNamed(context, '/pairs');
     }
   }
@@ -40,10 +36,9 @@ class _NavbarState extends State<Navbar> {
         iconSize: 35,
         fixedColor: Colors.black,
         items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(icon: Icon(Icons.settings),title: SizedBox(height: 0,),),
           BottomNavigationBarItem(icon: Icon(Icons.location_on),title: SizedBox( height: 0,),),
-          BottomNavigationBarItem(icon: Icon(Icons.person),title: SizedBox(height: 0,),),
           BottomNavigationBarItem(icon: Icon(Icons.favorite),title: SizedBox(height: 0,),),
+          BottomNavigationBarItem(icon: Icon(Icons.people),title: SizedBox(height: 0,),),
         ],
       )
     );
