1263fc6a4fe77c0f781efc59efa3f863d2716980
Alperen Sarinay <alperensarinay@gmail.com>
2020-06-20 17:30:37 +0300

    pairs :: bug fixed

--- lib/pages/pairs.dart
+++ lib/pages/pairs.dart
@@ -147,36 +147,38 @@ class _PairesPageState extends State<Pairs> {
                 ),
               )
             ),
-            Container(
-              height : 180,
-              child :Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),),
-                elevation: 5.0,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    const ListTile(title: Text('Kod Giriniz',style: TextStyle(fontSize : 20,fontWeight: FontWeight.w900),),),
-                    TextField(
-                      controller: receiverTokenCTRL,
-                      decoration: InputDecoration(
-                        icon: Icon(Icons.edit),
-                        border: InputBorder.none,
-                        hintText: 'Bağlantı kodunu giriniz.'
-                      ),
-                    ),
-                    ButtonBar(
-                      children: <Widget>[
-                        RaisedButton(
-                          onPressed: requestPair,
-                          color: Colors.blue[500],
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),),
-                          child: Center(child: Text("Gönder",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),),
+            if(checkPairStatus == false)(
+              Container(
+                height : 180,
+                child :Card(
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),),
+                  elevation: 5.0,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      const ListTile(title: Text('Kod Giriniz',style: TextStyle(fontSize : 20,fontWeight: FontWeight.w900),),),
+                      TextField(
+                        controller: receiverTokenCTRL,
+                        decoration: InputDecoration(
+                          icon: Icon(Icons.edit),
+                          border: InputBorder.none,
+                          hintText: 'Bağlantı kodunu giriniz.'
                         ),
-                      ],
-                    ),
-                  ],
+                      ),
+                      ButtonBar(
+                        children: <Widget>[
+                          RaisedButton(
+                            onPressed: requestPair,
+                            color: Colors.blue[500],
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0),),
+                            child: Center(child: Text("Gönder",style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold,),),),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+              )
             ),
             Container(
               height : 180,
