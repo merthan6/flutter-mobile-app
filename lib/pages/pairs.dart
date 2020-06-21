52ad05771b42a6de4df7fe5bef2349c9e9914673
atakde <mehata1997@hotmail.com>
2020-06-22 02:23:22 +0300

    Change error msg!

--- lib/pages/pairs.dart
+++ lib/pages/pairs.dart
@@ -277,7 +277,7 @@ class _PairesPageState extends State<Pairs> {
       if(response.statusCode == 200){
         var datauser = json.decode(response.body);
         if(datauser["data"]["success"] == false){
-           _showMyDialog("Böyle bir bağlantı kodu bulunamadı!");
+           _showMyDialog("Istek gönderilemedi! Böyle bir kod yok ya da bekleyen bir isteğiniz var!");
         } else {
           _showMyDialog("İstek Gönderildi!");
         }  
